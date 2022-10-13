53996dd0e989e1e722147bf150ecc6bebbfab3a7
MomollyWang <wang.xinwa@northeastern.edu>
2022-10-12 21:09:40 -0700

    test

---
+++ server/routes/posts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getPosts, createPosts, updatePost, deletePost } from '../controllers/posts.js';
+
+const router = express.Router();
+
+
+//http://localhost:3000/posts
+
+router.get('/', getPosts);
+router.post('/', createPosts);
+router.patch('/:id', updatePost)
+router.delete('/:id', deletePost)
+export default router;
